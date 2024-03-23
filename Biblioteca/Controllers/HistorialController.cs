9888f7148ad37ad78a1f8565ac02e24c5b4c7376
Juan <juandaortega0712@gmail.com>
2024-03-22 19:03:04 -0500

    se sube proyecto de biblioteca, prototipo y diagrama

---
+++ Biblioteca/Controllers/HistorialController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Biblioteca.Controllers
+{
+    public class HistorialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
---
+++ Biblioteca/Controllers/UsuariosController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+namespace Biblioteca.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Show()
+        {
+            return View();
+        }
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
