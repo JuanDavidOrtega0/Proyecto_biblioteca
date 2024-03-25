d05d0b89873703b27c6d85fb6cbc57954e941e83
Juan <juandaortega0712@gmail.com>
2024-03-24 20:00:20 -0500

    Modales para el CRUD de usuarios

--- Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a3a625a3d0d6054f6aa8f652af1fff754fdb3ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f7d3000e600eeace3c180c37b8b19c25ca4531")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
